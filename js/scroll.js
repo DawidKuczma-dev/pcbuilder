7e2d23f13fc9d2bdf550620f03a72c9e5f1a620d
Dawid Kuczma <dawidkuczma.dev@gmail.com>
2025-05-30 16:46:18 +0200

    Add "work in progress" overlay on second main image

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,13 @@ const sections = document.querySelectorAll('.target-section');
 images.forEach((image) => {
    image.addEventListener('click', () => {
       const targetId = image.dataset.target;
-
+      if (targetId === 'components') {
+         image.classList.add('in-progress');
+         // setTimeout(() => {
+         //    image.classList.remove('in-progress');
+         // }, 3000);
+         return;
+      }
       sections.forEach((section) => section.classList.remove('active'));
 
       const targetElement = document.getElementById(targetId);
